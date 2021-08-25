174593ed0a492f77eea64c40a683e3b19f8e8de8
espe-on <contact@josephcrawley.dev>
2021-08-25 15:21:40 +0100

    added storage values to fruit and veg

--- src/data/vegetables.ts
+++ src/data/vegetables.ts
@@ -4,106 +4,106 @@ export const vegetables : Vegetable[] = [
     {
         name: "Asparagus",
         months: ["April", "May", "June"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 4,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/fb1ff534-6cd5-4c3c-b9d3-1fe6c59a999f/Asparagus_PUB0028089.jpg"
     },
     {
         name: "Aubergines",
         months: ["August", "September"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 6,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/e19756b6-1f14-4495-8444-250d249b54d8/aubergine_PUB0021090.jpg"
     },
     {
         name: "Beetroot",
         months: ["June", "July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 4,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/895fa789-6e9c-45d9-90d6-87f8d0e30fa7/Harvesting-baby-beetroot_PUB0002432.jpg"
     },
     {
         name: "Broad beans",
         months: ["May", "June", "July"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/10312c22-d4f8-46fe-96db-a5df90b08f98/Broad_bean_PUB0002070.jpg"
     },
     {
         name: "Broccoli",
         months: ["February", "March", "April", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/59f5a144-3b58-4ef5-b3d5-660552fbf560/Brassica-oleracea-Italica-Group_ADV0000521.jpg"
     },
     {
         name: "Brussels sprouts",
         months: ["January", "February", "March", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/47aadc6a-0219-438d-88d9-83ecb59fa837/Brassica-oleracea-(Gemmifera-Group)-Stan-_WSY0025698.jpg"
     },
     {
         name: "Cabbage",
         months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 20,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/3c00ea55-b6e8-4404-89f9-600e5238b273/Cabbage_MAR0005357.jpg"
     },
     {
         name: "Calabrese",
         months: ["August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/13ed29f6-57d6-4eeb-85d5-1a243ca152b5/calabrese-express-940x627.jpg?width=940&height=627&ext=.jpg"
     },
     {
         name: "Carrots",
         months: ["May", "June", "July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 25,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/2e29792d-d556-4bee-a870-f9d657ca9635/Daucus-carota-Sweet-Candle-_PUB0012429.jpg"
     },
     {
         name: "Cauliflower",
         months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/71d0c405-03cb-4c2e-ae54-1dfbe4b0ea17/Cauliflower_PUB0004106.jpg"
     },
     {
         name: "Celeriac",
         months: ["January", "February", "March", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/12f78fdc-7252-4ab7-98a7-ce1a071142c4/celeriac.jpg"
     },
     {
         name: "Celery",
         months: ["August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/285e134e-9324-471d-87cc-3093036315e4/celery.jpg"
     },
     {
         name: "Chard",
         months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 10,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/0e17ca74-25c8-4d4c-a0ab-2e11984901d9/Rhubarb-chard940x627.jpg"
     },
     {
         name: "Chicory",
         months: ["January", "February", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/06c9a2cd-79c1-4d10-9a5e-2d60d8d45f78/chicory.jpg"
     },
     {
         name: "Chilli peppers",
         months: ["July", "August", "September", "October",],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/b6c8e0a5-cc01-4f18-9182-fc1da27d4cb3/chilli_pepper_MAR0010547.jpg"
     },
     {
@@ -116,275 +116,268 @@ export const vegetables : Vegetable[] = [
     {
         name: "Chinese cabbage",
         months: ["May", "June", "July", "August", "September", "October", "November",],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/f3c7e023-49a4-4255-af6e-075869aff43c/Chinese-cabbage940x627.jpg"
     },
     {
         name: "Courgettes",
         months: ["June", "July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/fe7cc5ce-ea4e-4fdd-b9a8-6f0bad33ca52/courgettes.jpg"
     },
     {
         name: "Cucumbers",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/4eefe3e7-0d21-4ea9-a74a-2df0409c9382/Cucumber-Swing-_WSYD0018485;Cucumber-Swing-;1876TN22.jpg"
     },
     {
         name: "Endive",
         months: ["March", "April", "May", "June", "July", "August", "September", "October", "November"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/de86b038-b4ce-4f49-b1d2-39e84deaf190/endive-Wallone3x2.jpg"
     },
     {
         name: "Fennel",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 10,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/30bb905c-61ce-4052-82a8-aef33a9de5d5/Florence-fennel940x627.jpg"
     },
     {
         name: "French beans",
         months: ["July", "August", "September"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/8b5897ad-58fe-457c-9105-a85c09b11681/Dwarf-french-bean571x321.jpg"
     },
     {
         name: "Garlic",
         months: ["June", "July", "August",],
-        daysFresh: "to add",
+        daysFresh: 14,
         storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/99ca41fe-a3ec-4edc-bd6c-79428c3df83c/Garlic-bulbs_MAR0002748.jpg"
     },
     {
         name: "Globe artichokes",
         months: ["June", "July", "August", "September"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 3,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/04f30dc2-8e9f-4824-9ceb-bbf97092adae/Globe-artichoke_PUB0002979.jpg"
     },
     {
         name: "Jerusalem artichokes",
         months: ["January", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/6fa3b3b3-0f41-4e82-8449-66d3ed63908b/Jerusalem-artichokes-940x627.jpg?width=940&height=627&ext=.jpg"
     },
     {
         name: "Kale",
         months: ["January", "February", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/aed086ce-cec8-44f2-851e-39953088106b/Curly-kale-(borecole)-%e2%80%98Reflex%e2%80%99-940x627.jpg"
     },
     {
         name: "Kohlrabi",
         months: ["May", "June", "July", "August", "September", "October", "November"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/a660b1ff-9e7a-48d7-a759-35e30e60c6ad/Kohlrabi-Kolibri-_WSYD0018490;Kohlrabi-Kolibri-;1008TN3.jpg"
     },
     {
         name: "Leeks",
         months: ["January", "February", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/08cf080f-5a71-4ece-9425-4c0f2062d61c/Allium-porrum-Bandit-_WSYD0005331.jpg"
     },
     {
         name: "Lettuce",
         months: ["May", "June", "July", "August", "September", "October", "November",],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/87eb29f6-29c5-4ba2-ab6c-cd9b1feeb51f/Lettuce_PUB0004183.jpg"
     },
     {
         name: "Marrow",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 60,
+        storage: Cupboard,
         imageUrl: "https://www.rhs.org.uk/getmedia/2366df46-0711-46ab-9430-d57d147a134d/A-collection-of-Cucurbita-pepo-(marrow)_PUB0020904.jpg"
     },
     {
         name: "Mizuna",
         months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/5448d1d0-788c-4bdc-a717-ea485c91ebb2/mizuna940x627.jpg"
     },
     {
         name: "Okra",
         months: ["July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 3,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/a06d8422-d1a4-439b-a184-e50663078f47/Okra-3x2.jpg"
     },
     {
         name: "Onions",
         months: ["July", "August", "September"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 60,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/36dce638-3fdd-423d-849f-5cb31161023a/Onion571x321.jpg"
     },
     {
         name: "Pak choi",
         months: ["May", "June", "July", "August", ],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/b909727f-9ed3-4ac2-9084-910e41db3ae5/Pak-choi-%e2%80%98Summer-Breeze%e2%80%99-940x627.jpg"
     },
     {
         name: "Parsnips",
         months: ["January", "February", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 21,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/d0b65739-401d-4163-9339-979514b15763/parsnip_WSYD0014255.jpg"
     },
     {
         name: "Peas",
         months: ["June", "July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 60,
+        storage: "Freezer",
         imageUrl: "https://www.rhs.org.uk/getmedia/9685aeb0-6d30-4ff5-9711-1d77ffc81652/Peas_WSYD0018492;Peas.jpg"
     },
     {
         name: "Peppers",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 10,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/e78db8ed-168a-4569-95fd-997fa287d5ce/Capsicum-display_PUB0007109.jpg"
     },
     {
         name: "Potatoes",
         months: ["June", "July", "August", "September", "October"],
-        daysFresh: 10,
+        daysFresh: 28,
         storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/556b1f9e-b456-4ba1-bcf3-bf9b908be955/potato-selection940x627.jpg"
     },
     {
         name: "Pumpkins",
         months: ["September", "October"],
-        daysFresh: "to add",
+        daysFresh: 60,
         storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/cdc20cb8-a1a7-4df2-b29b-87bfc533feef/Assorted-pumpkins-and-squashes_WSYD0017895.jpg"
     },
     {
         name: "Radishes",
         months: ["January", "February", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/db12e66c-6af7-4b8e-a7c0-573c219ccdcc/Radishes_PUB0004103.jpg"
     },
     {
         name: "Rocket",
         months: ["June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/ada7442e-1241-4dfb-a912-67fe5861b9cd/Salad-rocket940x627.jpg"
     },
     {
         name: "Runner beans",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/0d4902ff-7ba2-47b9-9637-2ca7d80070b5/Runner-beans-Phaseolus-coccineus-Liberty_WSYD0006493.jpg?width=940&height=627&ext=.jpg"
     },
     {
         name: "Salad leaves",
         months: ["May", "June", "July", "August", "September", "October", "November"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 5,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/d49f27d8-3c02-43d4-8d21-70975f7478c6/Salad-leaves-in-growing-bags-(l-to-r)-Mizuna-Kyoto-,-Pak-choi-Yuushou-,-Kale-Fizz-_PUB0021097.jpg"
     },
     {
         name: "Spring onions",
         months: ["March", "April", "May", "June", "July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/5baaf970-4140-4e31-8ebe-0c5958f135d7/salad-onions-940x627.jpg"
     },
     {
         name: "Salsify",
         months: ["March", "April", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/ba7ac8ba-e512-4813-8572-27eab2d382c5/Salsify940x627.jpg"
     },
     {
         name: "Shallots",
         months: [ "July", "August",],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 28,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/609c3b36-7923-4705-bc1e-39b5017d670c/Shallots-Allium-cepa-Aggregatum-Group_PUB0020235-3x2.jpg"
     },
     {
         name: "Soy beans",
         months: ["August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 60,
+        storage: "Cupboard" ,
         imageUrl: "https://www.rhs.org.uk/getmedia/8867082f-868a-40dc-b0c3-7605392a0a74/Glycine-max-(soya-bean)-pod_PUB0013431.jpg"
     },
     {
         name: "Spinach",
         months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 3,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/743086ee-1ff9-448f-bf4c-69f911765cfa/spinach-Emilia3x2.jpg"
     },
     {
         name: "Squash",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/f098c479-f700-410e-83fb-099eafbd68cf/Winter-squash-Golden-Hubbard-_RHS-0002072.jpg"
     },
     {
         name: "Swede",
         months: ["September", "October", "November", "December"],
-        daysFresh: "to add",
+        daysFresh: 14,
         storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/e3ed7124-ecfa-4efe-bc94-9f41d0e52279/SwedesMain.jpg"
     },
     {
         name: "Sweet potatoes",
         months: ["August", "September", "October"],
-        daysFresh: 10,
+        daysFresh: 14,
         storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/a28497fe-fcbd-4cad-b4aa-489cbab12722/Ipomoea-batatas940x627.jpg?width=940&height=627&ext=.jpg"
     },
     {
         name: "Sweetcorn",
         months: ["July", "August", "September", "October"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 7,
+        storage: "Fridge",
         imageUrl: "https://www.rhs.org.uk/getmedia/85aab220-0746-4fd4-96d2-7852c2f39113/Sweetcorn-in-the-Fruit-and-Vegetable-Competition-at-the-RHS-London-Harvest-Festival-Show-2012-_MAR0010664.jpg"
     },
     {
         name: "Tomatoes",
         months: ["July", "August", "September"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/88c682f4-a291-47ee-a5cb-282bd75680ec/Tomkin-British-Tomato-Growers-Association-National-Plant-Societies-Plant-Heritage-Marquee-Tatton-Park-2012-_PUB0024696.jpg"
     },
     {
         name: "Turnip",
         months: ["March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        daysFresh: "to add",
-        storage: "to add",
+        daysFresh: 14,
+        storage: "Cupboard",
         imageUrl: "https://www.rhs.org.uk/getmedia/9bf0ae1e-f532-4cab-87c8-19c6eadaa3ff/Turnips_PUB0019241.jpg"
-    },
-    {
-        name: "",
-        months: [],
-        daysFresh: "to add",
-        storage: "to add",
-        imageUrl: ""
     },
 ]
 
